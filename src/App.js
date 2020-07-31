2995118ed8e45f06ec05b239fd1d8727477df6cc
josh231101 <wiijosue333@gmail.com>
2020-07-30 19:49:17 -0500

    Add comment section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from "@material-ui/core/Modal";
 import {Button,Input} from "@material-ui/core";
 import ImageUpload from "./ImageUpload";
+import InstagramEmbed from 'react-instagram-embed';
+
 
 
 /**MATERIAL UI STYLES */
@@ -185,20 +187,40 @@ function App() {
 
       </div>
      
-    
-      {posts.map(({id,post}) => (
-        <Post 
-          key={id}
-          username={post.username} 
-          caption={post.caption}
-          imageUrl={post.imageUrl}
-        />
-      ))}
+      <div className="app__posts">
+        <div className="app__postsLeft">
+        {posts.map(({id,post}) => (
+          <Post 
+            key={id}
+            postId={id}
+            username={post.username} 
+            caption={post.caption}
+            imageUrl={post.imageUrl}
+          />
+        ))}
+        </div>
+        <div className="app__postsRight">
+          <InstagramEmbed
+            url='https://www.instagram.com/p/CCkM6SBJZOs/'
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+        </div>
+      </div>
+      
 
       {user?.displayName ? ( <ImageUpload username={user.displayName}/>) 
       : (
         <h3>Sorry you need to login to upload</h3>
       )}
+
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React,{useState , useEffect} from 'react'
 import "./Post.css";
+import { db } from "./firebase"
 import Avatar from "@material-ui/core/Avatar"
 
-function Post({username, caption, imageUrl}) {
+function Post({username, caption, imageUrl, postId}) {
+    const [comments, setComments] = useState([])
+    const [comment, setComment] = useState("")
+
+    useEffect(() =>{
+        let unsubscribe;
+        if(postId){
+            unsubscribe = db
+                .collection("users")
+                .doc(postId)
+                .collection("comments")
+                .onSnapshot((snapshot) => {
+                    setComments(snapshot.docs.map((docs) => docs.data()))
+                })
+        }
+        return () => {
+            unsubscribe()
+        }
+    },[postId])
+
+    const postComment = (event) =>{
+
+    }
+
+
     return (
         <div className="post">
             <div className="post__header">
@@ -16,6 +41,24 @@ function Post({username, caption, imageUrl}) {
             <img className="post__image" src={imageUrl} alt="User post image"/>
 
             <h4 className="post__text"><strong>{username} </strong>{caption}</h4>
+
+            <form className="post__commentBox">
+                <input
+                    className="post__input"
+                    type="text"
+                    placeholder="Add a comment..."
+                    onChange={(e)=> setComment(e.target.value)}
+                    value={comment}
+                />
+                <button
+                    className="post__button"
+                    disable={!comment}
+                    type="submit"
+                    onClick={postComment}
+                >
+                    Post
+                </button>
+            </form>
         </div>
     )
 }
